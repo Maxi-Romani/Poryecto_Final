cdb6f85486e5121ba580e18c65f7a5d97678fae7
Maxi Romani <romanimaxi@gmail.com>
2022-06-04 17:13:10 -0300

    segundo commit

--- proyecto_final/proyecto_app/urls.py
+++ proyecto_final/proyecto_app/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
-from proyecto_app.views import Productos, Usuarios, Descripcion
+from proyecto_app.views import productos, usuarios, descripcion
 
+urlpatterns = [
+    path("productos/", productos, name="productos"),
+    path("usuarios/", usuarios, name="usuarios"),
+    path("descripcion/", descripcion, name="descripcion")
+]
--- proyecto_final/proyecto_app/views.py
+++ proyecto_final/proyecto_app/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from proyecto_app.models import Productos
+from proyecto_app.models import Productos, Usuarios, Descripcion
 
 # Create your views here.
 
-def proyecto_app(request):
+def productos(request):
     print(request.method)
     productos = Productos.objects.all()
     context = {'productos':productos}
-    return render(request, 'products.html', context=context)
-    
+    return render(request, 'productos.html', context=context)
+
+def usuarios(request):
+    print(request.method)
+    usuarios = Usuarios.objects.all()
+    context = {'usuarios':usuarios}
+    return render(request, 'usuarios.html', context=context)
+
+def descripcion(request):
+    print(request.method)
+    descripcion = Descripcion.objects.all()
+    context = {'descripcion':descripcion}
+    return render(request, 'descripcion.html', context=context)
+
